d8c51bb49fad3a69d5626a51435d4648bfe8c467
Oliver Holdaway <holdawayo@cardiff.ac.uk>
2022-02-10 18:32:17 +0000

    takes request body as hashmap so data can be easily selected

--- api/src/main/java/com/uas/api/controller/AircraftController.java
+++ api/src/main/java/com/uas/api/controller/AircraftController.java
@@ -1,19 +1,26 @@
 package com.uas.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.uas.api.entities.Aircraft;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/aircraft")
 public class AircraftController {
 
     @PostMapping(value = "/add", consumes = "application/json")
-    void addAircraft(@RequestBody String test){
-
-        //{"tailNumber":"G-101","location":"London","platformStatus":"Operational","platformType":"Platform A"}
-        System.out.println(test);
+    void addAircraft(@RequestBody HashMap<String, String> requestData){
+        System.out.println(requestData.get("tailNumber"));
+        System.out.println(requestData.get("location"));
+        System.out.println(requestData.get("platformStatus"));
+        System.out.println(requestData.get("platformType"));
+        
     }
 }
