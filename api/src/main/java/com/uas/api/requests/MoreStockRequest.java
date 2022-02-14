4bb3e197ff4fd6150dfaceb87cf125312eeb9c54
Sophie Bareham (uni) <BarehamSA@cardiff.ac.uk>
2022-02-14 11:18:32 +0000

    Updated stock request

--- api/src/main/java/com/uas/api/requests/MoreStockRequest.java
+++ api/src/main/java/com/uas/api/requests/MoreStockRequest.java
@@ -1,4 +1,21 @@
 package com.uas.api.requests;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MoreStockRequest {
+    private String location;
+    private double cost;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private Date orderDate;
+    private ArrayList<Integer> partTypes;
+    private ArrayList<Integer> quantities;
 }
