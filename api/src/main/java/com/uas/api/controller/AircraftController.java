ddd3c5ea57998e8d4091fa8f75635be9a1ebafa7
Oliver Holdaway <holdawayo@cardiff.ac.uk>
2022-02-16 18:58:25 +0000

    feat : issue #2 added responses and error messages that are returned in the body from the post request

--- api/src/main/java/com/uas/api/controller/AircraftController.java
+++ api/src/main/java/com/uas/api/controller/AircraftController.java
@@ -8,6 +8,8 @@ import com.uas.api.services.AircraftService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -20,7 +22,7 @@ import java.util.Optional;
 public class AircraftController {
 
     private final AircraftService aircraftService;
-    private static final Logger log = LoggerFactory.getLogger(AircraftController.class);
+
 
     @Autowired
     public AircraftController(LocationRepository locationRepository, AircraftService aircraftService, AircraftRepository aircraftRepository) {
@@ -29,18 +31,17 @@ public class AircraftController {
 
 
     @PostMapping(value = "/add", consumes = "application/json")
-    void addAircraft(@RequestBody HashMap<String, String> requestData) throws Exception {
+    ResponseEntity<?> addAircraft(@RequestBody HashMap<String, String> requestData) throws Exception {
         //takes data as hashmap to manually create aircraft as couldn't automatically create it from the json
         // as enums weren't created from the json strings.
 
-        log.info("Aircraft added by user. Tailnumber:"+requestData.get("tailNumber")
-                +" Location:" +requestData.get("location")
-                +" Platform Status:"+requestData.get("platformStatus")
-                +" Platform Type:"+requestData.get("platformType"));
+        String result = aircraftService.addAircraftFromJson(requestData);
 
-        boolean result = aircraftService.addAircraftFromJson(requestData);
-
-
+        if(result == null){
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } else {
+            return ResponseEntity.badRequest().body("Error adding aircraft. "+result);
+        }
     }
 
 }
--- api/src/main/java/com/uas/api/services/AircraftService.java
+++ api/src/main/java/com/uas/api/services/AircraftService.java
@@ -1,11 +1,14 @@
 package com.uas.api.services;
 
+import com.uas.api.controller.AircraftController;
 import com.uas.api.models.entities.Aircraft;
 import com.uas.api.models.entities.Location;
 import com.uas.api.models.entities.enums.PlatformStatus;
 import com.uas.api.models.entities.enums.PlatformType;
 import com.uas.api.repositories.AircraftRepository;
 import com.uas.api.repositories.LocationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,7 @@ import java.util.Optional;
 public class AircraftService {
     private final AircraftRepository aircraftRepository;
     private final LocationRepository locationRepository;
+    private static final Logger log = LoggerFactory.getLogger(AircraftService.class);
 
     @Autowired
     public AircraftService(AircraftRepository aircraftRepository, LocationRepository locationRepository) {
@@ -28,8 +32,8 @@ public class AircraftService {
         aircraftRepository.save(aircraft);
     }
 
-    public boolean addAircraftFromJson(HashMap<String,String> requestData) throws Exception {
-        boolean error = false;
+    public String addAircraftFromJson(HashMap<String,String> requestData) throws Exception {
+        String errorMessage = null;
 
         PlatformStatus platformStatus = PlatformStatus.DESIGN;
         switch (requestData.get("platformStatus")) {
@@ -37,31 +41,33 @@ public class AircraftService {
             case "Production" : platformStatus = PlatformStatus.PRODUCTION; break;
             case "Operation" : platformStatus = PlatformStatus.OPERATION; break;
             case "Repair" : platformStatus = PlatformStatus.REPAIR; break;
-            default: error = true; throw new Exception("Invalid platform status.");
+            default: errorMessage = "Invalid platform status."; break;
         }
 
         Optional<Location> location = locationRepository.findLocationByLocationName(requestData.get("location"));
         if(location.isEmpty()){
-            error = true;
+            errorMessage = "Location not found.";
         }
 
         PlatformType platformType = PlatformType.PLATFORM_A;
         switch (requestData.get("platformType")) {
             case "Platform_A" : break;
             case "Platform_B" : platformType = PlatformType.PLATFORM_B; break;
-            default: error = true; break;
+            default: errorMessage = "Invalid platform type."; break;
         }
 
-        if(!error) {
+        if(errorMessage == null) {
             Aircraft aircraft = new Aircraft(requestData.get("tailNumber"), location.get(), platformStatus, platformType);
             try {
                 aircraftRepository.save(aircraft);
+                log.info("Aircraft added by user. Tailnumber:"+requestData.get("tailNumber")
+                        +" Location:" +requestData.get("location")
+                        +" Platform Status:"+requestData.get("platformStatus")
+                        +" Platform Type:"+requestData.get("platformType"));
             } catch(Exception e) {
-                System.out.println(e);
+                errorMessage = e.getMessage();
             }
-
         }
-
-        return error;
+        return errorMessage;
     }
 }
