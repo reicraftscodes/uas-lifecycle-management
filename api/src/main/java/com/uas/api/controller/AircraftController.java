de40fe2d4fbe30843abbcc5ec0bfb17446d705f3
Oliver Holdaway <holdawayo@cardiff.ac.uk>
2022-02-16 19:53:46 +0000

    docs : issue #2 added comments and reformated to pass checkstyle in the aircraft controller class

--- api/src/main/java/com/uas/api/controller/AircraftController.java
+++ api/src/main/java/com/uas/api/controller/AircraftController.java
@@ -1,47 +1,44 @@
 package com.uas.api.controller;
 
-
-import com.uas.api.models.entities.Aircraft;
-import com.uas.api.repositories.AircraftRepository;
-import com.uas.api.repositories.LocationRepository;
 import com.uas.api.services.AircraftService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/aircraft")
 public class AircraftController {
-
+    /**
+     * Aircraft service used to communicate with the db about the aircraft table.
+     */
     private final AircraftService aircraftService;
 
-
+    /**
+     * Constructor.
+     * @param aircraftService Aircraft service for db communication.
+     */
     @Autowired
-    public AircraftController(LocationRepository locationRepository, AircraftService aircraftService, AircraftRepository aircraftRepository) {
+    public AircraftController(final AircraftService aircraftService) {
         this.aircraftService = aircraftService;
     }
 
-
+    /**
+     * Postmapping for requests to add aircraft to the database.
+     * @param requestData JSON data from the request body turned into a HashMap.
+     * @return A response to the request with either a confirmation or the error received.
+     */
     @PostMapping(value = "/add", consumes = "application/json")
-    ResponseEntity<?> addAircraft(@RequestBody HashMap<String, String> requestData) throws Exception {
+    ResponseEntity<?> addAircraft(@RequestBody final HashMap<String, String> requestData) {
         //takes data as hashmap to manually create aircraft as couldn't automatically create it from the json
         // as enums weren't created from the json strings.
-
         String result = aircraftService.addAircraftFromJson(requestData);
 
-        if(result == null){
+        if (result == null) {
             return new ResponseEntity<>(null, HttpStatus.OK);
         } else {
-            return ResponseEntity.badRequest().body("Error adding aircraft. "+result);
+            return ResponseEntity.badRequest().body("Error adding aircraft. " + result);
         }
     }
-
 }
