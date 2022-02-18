379323c13501783462bf5e97f079997cb9ddbdf0
May <sanejolm@cardiff.ac.uk>
2022-02-18 12:30:13 +0000

    feat: issue #10 added 3 models, Enum Role, role and user

---
+++ api/src/main/java/com/uas/api/models/auth/ERole.java
@@ -0,0 +1,7 @@
+package com.uas.api.models.auth;
+
+public enum ERole {
+    ROLE_USER_LOGISTIC,
+    ROLE_USER_CTO,
+    ROLE_USER_CEO
+}
---
+++ api/src/main/java/com/uas/api/models/auth/User.java
@@ -0,0 +1,74 @@
+package com.uas.api.models.auth;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    @NotBlank
+    @Size(max = 50)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 50)
+    private String lastName;
+
+    @NotBlank
+    @Size(max = 50)
+    private String resetPasswordToken;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String username,
+                String email,
+                String password,
+                String firstName,
+                String lastName,
+                Set<Role> roles,
+                String resetPasswordToken) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.roles = roles;
+        this.resetPasswordToken = resetPasswordToken;
+    }
+}
