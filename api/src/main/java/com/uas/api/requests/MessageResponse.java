78fcff6eda26316f7b1e6328782e221019963444
May <sanejolm@cardiff.ac.uk>
2022-02-18 13:48:18 +0000

    fix: issue #10 add validation error response.

---
+++ api/src/main/java/com/uas/api/requests/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.uas.api.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class MessageResponse {
+    private String message;
+
+}
