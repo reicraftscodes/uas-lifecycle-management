d7feadb502c80281636dad853461701fd0fd1132
May <sanejolm@cardiff.ac.uk>
2022-02-21 15:24:30 +0000

    docs: issue #10 Requests - ErrorResponse and SignupRequest

--- api/src/main/java/com/uas/api/requests/ErrorResponse.java
+++ api/src/main/java/com/uas/api/requests/ErrorResponse.java
@@ -1,29 +1,54 @@
 package com.uas.api.requests;
 
 public class ErrorResponse {
+
+    /**
+     * Error response message
+     */
     private String message;
+
+    /**
+     * Error response status
+     */
     private String status;
 
+    /**
+     * Error response constructor
+     */
     public ErrorResponse() {
     }
 
+    /**
+     * Error response constructor
+     */
     public ErrorResponse(String message, String status) {
         this.message = message;
         this.status = status;
     }
 
+    /**
+     * Get error response message
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Set error response message
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Get error response status
+     */
     public String getStatus() {
         return status;
     }
-
+    /**
+     * Set error response status
+     */
     public void setStatus(String status) {
         this.status = status;
     }
--- api/src/main/java/com/uas/api/requests/SignupRequest.java
+++ api/src/main/java/com/uas/api/requests/SignupRequest.java
@@ -8,10 +8,29 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignupRequest {
+
+    /**
+     * Sign up email.
+     */
     private String email;
+    /**
+     * Sign up email confirmation.
+     */
     private String confirmEmail;
+    /**
+     * Sign up password.
+     */
     private String password;
+    /**
+     * Sign up password email verification.
+     */
     private String confirmPassword;
+    /**
+     * Sign up user first name.
+     */
     private String firstName;
+    /**
+     * Sign up user last name.
+     */
     private String lastName;
 }
