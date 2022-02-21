5b773f124ff75a4832dffb447c4fdd862d1fd1cc
May <sanejolm@cardiff.ac.uk>
2022-02-21 15:44:28 +0000

    docs and refactor: issue #10 Requests - refactor jwt reponse class and added documentation.

--- api/src/main/java/com/uas/api/response/JwtResponse.java
+++ api/src/main/java/com/uas/api/response/JwtResponse.java
@@ -1,62 +1,35 @@
 package com.uas.api.response;
 
+import lombok.*;
+
 import java.util.List;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtResponse {
+    /**
+     * Unique token.
+     */
     private String token;
+    /**
+     * Id.
+     */
     private Long id;
+    /**
+     * User name.
+     */
     private String username;
+    /**
+     * User email.
+     */
     private String email;
+    /**
+     * List of user Roles.
+     */
     private List<String> roles;
 
-    public JwtResponse() {
-    }
 
-    public JwtResponse(String token, Long id, String username, String email, List<String> roles) {
-        this.token = token;
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.roles = roles;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
 }
