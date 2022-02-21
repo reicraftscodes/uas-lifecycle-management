55ede3d626052f062f032277a590056900c84661
May <sanejolm@cardiff.ac.uk>
2022-02-21 15:06:17 +0000

    docs: issue #10 update UserDTO

--- api/src/main/java/com/uas/api/models/dtos/UserDTO.java
+++ api/src/main/java/com/uas/api/models/dtos/UserDTO.java
@@ -8,9 +8,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class UserDTO {
+
+    /**
+     * user id
+     */
     private Long id;
+    /**
+     * user user name
+     */
     private String username;
+    /**
+     * user email
+     */
     private String email;
+    /**
+     * user first name
+     */
     private String firstName;
+    /**
+     * user last name
+     */
     private String lastName;
 }
