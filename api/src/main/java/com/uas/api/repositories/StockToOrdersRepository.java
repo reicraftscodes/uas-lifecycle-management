72b6e6f2daa412177c80b093e267a0b1fb02f1ad
Oliver Holdaway <holdawayo@cardiff.ac.uk>
2022-03-23 17:17:52 +0000

    feat : issue #35 Added pdf content to invoice creator, it now shows a logo, address for delivery, and a table with the quantity and parts being ordered

--- api/src/main/java/com/uas/api/repositories/StockToOrdersRepository.java
+++ api/src/main/java/com/uas/api/repositories/StockToOrdersRepository.java
@@ -2,6 +2,13 @@ package com.uas.api.repositories;
 
 import com.uas.api.models.entities.StockToOrders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StockToOrdersRepository extends JpaRepository<StockToOrders, Integer> {
+
+    @Query(value="SELECT * FROM stocktoorders WHERE OrderID=:orderID",nativeQuery = true)
+    List<StockToOrders> findAllByOrderID(@Param("orderID") long OrderID);
 }
--- api/src/main/java/com/uas/api/services/InvoiceServiceImpl.java
+++ api/src/main/java/com/uas/api/services/InvoiceServiceImpl.java
@@ -1,39 +1,99 @@
 package com.uas.api.services;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Header;
-import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.uas.api.models.entities.Orders;
-import com.uas.api.requests.MoreStockRequest;
+import com.uas.api.models.entities.StockToOrders;
+import com.uas.api.repositories.StockToOrdersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.FileOutputStream;
+import java.util.List;
 
 
 @Service
 public class InvoiceServiceImpl implements InvoiceService {
 
+    private final StockToOrdersRepository stockToOrdersRepository;
+
+    @Autowired
+    public InvoiceServiceImpl(StockToOrdersRepository stockToOrdersRepository) {
+        this.stockToOrdersRepository = stockToOrdersRepository;
+    }
+
     @Override
     public boolean generatePDF(Orders givenOrder){
-        String fileName = "src/main/resources/order.pdf";
+        String fileName = "src/main/resources/invoices/order_"+givenOrder.getOrderID()+".pdf";
         Document document = new Document();
-
-        System.out.println(givenOrder.getOrderID());
-        System.out.println(givenOrder.getLocationName());
-        System.out.println(givenOrder.getOrderDateTime());
-        System.out.println(givenOrder.getSupplierEmail());
-        System.out.println(givenOrder.getTotalCost());
 
         try {
             PdfWriter.getInstance(document, new FileOutputStream(fileName));
             document.open();
 
-            document.add(new Paragraph("Order invoice"));
+            Font boldFont = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
+
+            Image img = Image.getInstance("src/main/resources/img/logo.png");
+            img.scaleAbsolute(160,50);
+            img.setAbsolutePosition(10,782);
+            document.add(img);
+
+            Paragraph orderNum = new Paragraph("\n\nOrder #"+givenOrder.getOrderID(), boldFont);
+            orderNum.setAlignment(Element.ALIGN_RIGHT);
+            document.add(orderNum);
+
+            Paragraph addressLabel = new Paragraph("\nShip to:",boldFont);
+            addressLabel.setAlignment(Element.ALIGN_RIGHT);
+            document.add(addressLabel);
+
+            if(givenOrder.getLocationName().getAddressLine2()==null){
+                givenOrder.getLocationName().setAddressLine2("");
+            } else {
+                givenOrder.getLocationName().setAddressLine2("\n"+givenOrder.getLocationName().getAddressLine2());
+            }
+            Paragraph address = new Paragraph(
+                    "Sierra Nevada Corporation Mission Systems UK \n"
+                    +givenOrder.getLocationName().getAddressLine1()
+                    +givenOrder.getLocationName().getAddressLine2()+"\n"
+                    +givenOrder.getLocationName().getPostcode()+"\n"
+                    +givenOrder.getLocationName().getCountry()+"\n\n");
+            address.setAlignment(Element.ALIGN_RIGHT);
+            document.add(address);
+
+            float[] columnWidths = {5f,5f,5f};
+
+            PdfPTable table = new PdfPTable(columnWidths);
+            table.setWidthPercentage(100);
+            table.addCell("Part Number");
+            table.addCell("Part Name");
+            table.addCell("Quantity");
+            table.completeRow();
+
+            List<StockToOrders> totalOrder = stockToOrdersRepository.findAllByOrderID(givenOrder.getOrderID());
+            for(StockToOrders order : totalOrder){
+                table.addCell("\n"+order.getPartID().getId().toString());
+                table.addCell("\n"+order.getPartID().getPartName().getName());
+                table.addCell("\n"+order.getQuantity());
+                table.completeRow();
+            }
+
+
+
+            document.add(table);
+
+
+
 
             document.close();
+
+
+
+
+
             return true;
         } catch (Exception e) {
+            System.out.println(e);
             return false;
         }
 
