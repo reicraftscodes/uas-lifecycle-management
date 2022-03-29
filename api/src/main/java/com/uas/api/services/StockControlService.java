1f12e7491ddf83658bd70f2b49138cf9095572cb
Hannah <bonnetthf@cardiff.ac.uk>
2022-03-29 16:58:44 +0100

    feat: issue #51 - Get a list of all previous stock orders.

--- api/src/main/java/com/uas/api/services/StockControlService.java
+++ api/src/main/java/com/uas/api/services/StockControlService.java
@@ -1,6 +1,9 @@
 package com.uas.api.services;
 
+import com.uas.api.models.dtos.StockOrderDTO;
 import com.uas.api.requests.MoreStockRequest;
+
+import java.util.List;
 
 public interface StockControlService {
     /**
@@ -9,4 +12,6 @@ public interface StockControlService {
      * @return the stock order.
      */
     StockControlServiceImpl.StockReceipt addMoreStock(MoreStockRequest moreStockRequest);
+
+    List<StockOrderDTO> getAllPreviousStockOrders();
 }
